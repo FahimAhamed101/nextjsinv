f0bd1137ecce4dd080b734f04e1f379a738b12e4
fahim <fahimahamedweb@gmail.com>
2025-01-17 22:59:28 +0600

    setup done for vercel

--- prisma/migrations/20250115225328_dev/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Product" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "category" TEXT NOT NULL,
-    "images" TEXT NOT NULL
-);
